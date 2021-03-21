8b1adc46146a670c3051bef485cee311e08b6b1c
Bogdan <BogKowIt@gmail.com>
2021-03-21 10:54:16 +0100

    add image support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes.jsx"
+import sword from "../images/swc-sword.png"
+import swordSVG from "../images/sword.svg"
 
 import React from 'react';
 
@@ -11,9 +13,10 @@ const App = () => {
             <section>
                 <h1>Oh Hai redact</h1>
             </section>
+            <img src={sword} alt="sword" width="250px"/>
+            <img src={swordSVG} alt="sword" width="250px"/>
+            <Recipes />
         </main>
-
-        <Recipes />
         </>
     );
 };
